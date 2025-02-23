12d6127639742dacd880edcb03809dc571ece7ce
IsaacR-001 <isaac.ramos@g.austincc.edu>
2025-02-23 19:45:05 +0000

    hw3

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,35 @@
-//write include statements
+#include <iostream>
+#include "decisions.h"
 
-int main() 
-{
-	return 0;
+int main() {
+    int choice;
+    int grade;
+
+    std::cout << "MAIN MENU\n";
+    std::cout << "1-Letter grade using if\n";
+    std::cout << "2-Letter grade using switch\n";
+    std::cout << "3-Exit\n";
+    std::cout << "Enter your choice: ";
+    std::cin >> choice;
+
+    if (choice == 1 || choice == 2) {
+        std::cout << "Enter a numerical grade (0-100): ";
+        std::cin >> grade;
+
+        if (grade >= 0 && grade <= 100) {
+            if (choice == 1) {
+                std::cout << "Letter grade: " << get_letter_grade_using_if(grade) << "\n";
+            } else if (choice == 2) {
+                std::cout << "Letter grade: " << get_letter_grade_using_switch(grade) << "\n";
+            }
+        } else {
+            std::cout << "Number is out of range.\n";
+        }
+    } else if (choice == 3) {
+        std::cout << "Exiting...\n";
+    } else {
+        std::cout << "Invalid choice.\n";
+    }
+
+    return 0;
 }
