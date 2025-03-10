04c364a6e1bb13e0dd22246b180fa11c88b9e2a5
IsaacR-001 <isaac.ramos@g.austincc.edu>
2025-03-10 02:56:01 +0000

    hw5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,32 @@
 //add include statements
+#include "func.h"
 
 //add function code here
+double get_gc_content(const string& dna) {
+    int gc_count = 0;
+    for (char ch : dna) {
+        if (ch == 'G' || ch == 'C') {
+            gc_count++;
+        }
+    }
+    return static_cast<double>(gc_count) / dna.length();
+}
+
+string get_reverse_string(string dna) {
+    string reversed;
+    for (int i = dna.length() - 1; i >= 0; i--) {
+        reversed += dna[i];
+    }
+    return reversed;
+}
+
+string get_dna_complement(string dna) {
+    dna = get_reverse_string(dna);
+    for (char& ch : dna) {
+        if (ch == 'A') ch = 'T';
+        else if (ch == 'T') ch = 'A';
+        else if (ch == 'C') ch = 'G';
+        else if (ch == 'G') ch = 'C';
+    }
+    return dna;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,33 @@
+#include <iostream>
+#include "func.h"
 
-int main() 
-{
+int main() {
+    int choice;
+    string dna;
 
-	return 0;
+    cout << "Menu:\n";
+    cout << "1- Get GC Content\n";
+    cout << "2- Get DNA Complement\n";
+    cout << "3- Exit\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    if (choice == 1 || choice == 2) {
+        cout << "Enter a DNA string: ";
+        cin >> dna;
+
+        if (choice == 1) {
+            double gc_content = get_gc_content(dna);
+            cout << "GC Content: " << gc_content << endl;
+        } else if (choice == 2) {
+            string complement = get_dna_complement(dna);
+            cout << "DNA Complement: " << complement << endl;
+        }
+    } else if (choice == 3) {
+        cout << "Exiting program.\n";
+    } else {
+        cout << "Invalid choice.\n";
+    }
+
+    return 0;
 }
